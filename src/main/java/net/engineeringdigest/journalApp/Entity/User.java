444fcc55fcad3f4a4366b9edbfad97ae9a4d501b
Majid <majidabduluop@gmail.com>
2024-09-24 18:27:30 -0400

    Second Commit

--- src/main/java/net/engineeringdigest/journalApp/Entity/User.java
+++ src/main/java/net/engineeringdigest/journalApp/Entity/User.java
@@ -11,7 +11,7 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import java.util.ArrayList;
 import java.util.List;
 
-@Document(collation = "users")
+@Document(collation = "userss")
 @Data
 public class User {
 
